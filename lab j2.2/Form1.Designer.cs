a931ff3ec055922d46bd0ce1fc3aeebc6627873a
onyamon <onyamon.p@Kkumail.com>
2023-12-06 13:40:50 +0700

    Initial commit

---
+++ lab j2.2/Form1.Designer.cs
@@ -0,0 +1,530 @@
+﻿namespace lab_j2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            tbinputNAME = new TextBox();
+            tbinputScore = new TextBox();
+            button1 = new Button();
+            tbinputID = new TextBox();
+            grad = new GroupBox();
+            label18 = new Label();
+            label17 = new Label();
+            label16 = new Label();
+            label15 = new Label();
+            label14 = new Label();
+            tbIDScore1 = new TextBox();
+            tbNameScore1 = new TextBox();
+            tbSCORE1 = new TextBox();
+            tbSCORE2 = new TextBox();
+            tbAVER = new TextBox();
+            tbNameScore2 = new TextBox();
+            tbIDScore2 = new TextBox();
+            label10 = new Label();
+            javer = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            tbgB1 = new TextBox();
+            tbgB2 = new TextBox();
+            tbgC1 = new TextBox();
+            tbgC2 = new TextBox();
+            tbgD1 = new TextBox();
+            tbgD2 = new TextBox();
+            tbgF = new TextBox();
+            label1 = new Label();
+            tbgA = new TextBox();
+            groupBox1.SuspendLayout();
+            grad.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(label13);
+            groupBox1.Controls.Add(tbinputNAME);
+            groupBox1.Controls.Add(tbinputScore);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(tbinputID);
+            groupBox1.ImeMode = ImeMode.Disable;
+            groupBox1.Location = new Point(12, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(411, 655);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "กรอกข้อมูล";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label11.Location = new Point(25, 222);
+            label11.Name = "label11";
+            label11.Size = new Size(56, 20);
+            label11.TabIndex = 20;
+            label11.Text = "คะแนน";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label12.Location = new Point(25, 143);
+            label12.Name = "label12";
+            label12.Size = new Size(91, 20);
+            label12.TabIndex = 21;
+            label12.Text = "ชื่อ-นามสกุล";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label13.Location = new Point(25, 73);
+            label13.Name = "label13";
+            label13.Size = new Size(93, 20);
+            label13.TabIndex = 22;
+            label13.Text = "รหัสนักศึกษา";
+            // 
+            // tbinputNAME
+            // 
+            tbinputNAME.BackColor = SystemColors.GradientActiveCaption;
+            tbinputNAME.Location = new Point(156, 136);
+            tbinputNAME.Name = "tbinputNAME";
+            tbinputNAME.Size = new Size(125, 27);
+            tbinputNAME.TabIndex = 4;
+            // 
+            // tbinputScore
+            // 
+            tbinputScore.BackColor = SystemColors.GradientActiveCaption;
+            tbinputScore.Location = new Point(156, 215);
+            tbinputScore.Name = "tbinputScore";
+            tbinputScore.Size = new Size(125, 27);
+            tbinputScore.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DimGray;
+            button1.ForeColor = Color.Cornsilk;
+            button1.Location = new Point(172, 274);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "บันทึก";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // tbinputID
+            // 
+            tbinputID.BackColor = SystemColors.GradientActiveCaption;
+            tbinputID.Location = new Point(156, 66);
+            tbinputID.Name = "tbinputID";
+            tbinputID.Size = new Size(125, 27);
+            tbinputID.TabIndex = 0;
+            // 
+            // grad
+            // 
+            grad.Controls.Add(label18);
+            grad.Controls.Add(label17);
+            grad.Controls.Add(label16);
+            grad.Controls.Add(label15);
+            grad.Controls.Add(label14);
+            grad.Controls.Add(tbIDScore1);
+            grad.Controls.Add(tbNameScore1);
+            grad.Controls.Add(tbSCORE1);
+            grad.Controls.Add(tbSCORE2);
+            grad.Controls.Add(tbAVER);
+            grad.Controls.Add(tbNameScore2);
+            grad.Controls.Add(tbIDScore2);
+            grad.Controls.Add(label10);
+            grad.Controls.Add(javer);
+            grad.Controls.Add(label9);
+            grad.Controls.Add(label8);
+            grad.Controls.Add(label7);
+            grad.Controls.Add(label6);
+            grad.Controls.Add(label5);
+            grad.Controls.Add(label4);
+            grad.Controls.Add(label3);
+            grad.Controls.Add(label2);
+            grad.Controls.Add(tbgB1);
+            grad.Controls.Add(tbgB2);
+            grad.Controls.Add(tbgC1);
+            grad.Controls.Add(tbgC2);
+            grad.Controls.Add(tbgD1);
+            grad.Controls.Add(tbgD2);
+            grad.Controls.Add(tbgF);
+            grad.Controls.Add(label1);
+            grad.Controls.Add(tbgA);
+            grad.Location = new Point(442, 39);
+            grad.Name = "grad";
+            grad.Size = new Size(917, 655);
+            grad.TabIndex = 1;
+            grad.TabStop = false;
+            grad.Text = "ผลลัพท์";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.Location = new Point(656, 175);
+            label18.Name = "label18";
+            label18.Size = new Size(89, 20);
+            label18.TabIndex = 31;
+            label18.Text = "คะแนนเฉลี่ย";
+            label18.Click += label18_Click;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label17.Location = new Point(333, 122);
+            label17.Name = "label17";
+            label17.Size = new Size(93, 20);
+            label17.TabIndex = 30;
+            label17.Text = "คะแนนต่ำสุด";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label16.Location = new Point(333, 66);
+            label16.Name = "label16";
+            label16.Size = new Size(91, 20);
+            label16.TabIndex = 29;
+            label16.Text = "คะแนนสูงสุด";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.Location = new Point(437, 23);
+            label15.Name = "label15";
+            label15.Size = new Size(93, 20);
+            label15.TabIndex = 28;
+            label15.Text = "รหัสนักศึกษา";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(611, 23);
+            label14.Name = "label14";
+            label14.Size = new Size(91, 20);
+            label14.TabIndex = 27;
+            label14.Text = "ชื่อ-นามสกุล";
+            // 
+            // tbIDScore1
+            // 
+            tbIDScore1.BackColor = SystemColors.GradientActiveCaption;
+            tbIDScore1.Location = new Point(420, 66);
+            tbIDScore1.Name = "tbIDScore1";
+            tbIDScore1.Size = new Size(125, 27);
+            tbIDScore1.TabIndex = 26;
+            // 
+            // tbNameScore1
+            // 
+            tbNameScore1.BackColor = SystemColors.GradientActiveCaption;
+            tbNameScore1.Location = new Point(589, 66);
+            tbNameScore1.Name = "tbNameScore1";
+            tbNameScore1.Size = new Size(125, 27);
+            tbNameScore1.TabIndex = 25;
+            // 
+            // tbSCORE1
+            // 
+            tbSCORE1.BackColor = SystemColors.GradientActiveCaption;
+            tbSCORE1.Location = new Point(752, 70);
+            tbSCORE1.Name = "tbSCORE1";
+            tbSCORE1.Size = new Size(125, 27);
+            tbSCORE1.TabIndex = 24;
+            // 
+            // tbSCORE2
+            // 
+            tbSCORE2.BackColor = SystemColors.GradientActiveCaption;
+            tbSCORE2.Location = new Point(752, 119);
+            tbSCORE2.Name = "tbSCORE2";
+            tbSCORE2.Size = new Size(125, 27);
+            tbSCORE2.TabIndex = 23;
+            // 
+            // tbAVER
+            // 
+            tbAVER.BackColor = Color.FromArgb(192, 255, 192);
+            tbAVER.Location = new Point(752, 168);
+            tbAVER.Name = "tbAVER";
+            tbAVER.Size = new Size(125, 27);
+            tbAVER.TabIndex = 22;
+            // 
+            // tbNameScore2
+            // 
+            tbNameScore2.BackColor = SystemColors.GradientActiveCaption;
+            tbNameScore2.Location = new Point(589, 119);
+            tbNameScore2.Name = "tbNameScore2";
+            tbNameScore2.Size = new Size(125, 27);
+            tbNameScore2.TabIndex = 21;
+            // 
+            // tbIDScore2
+            // 
+            tbIDScore2.BackColor = SystemColors.GradientActiveCaption;
+            tbIDScore2.Location = new Point(420, 119);
+            tbIDScore2.Name = "tbIDScore2";
+            tbIDScore2.Size = new Size(125, 27);
+            tbIDScore2.TabIndex = 20;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(788, 23);
+            label10.Name = "label10";
+            label10.Size = new Size(56, 20);
+            label10.TabIndex = 19;
+            label10.Text = "คะแนน";
+            // 
+            // javer
+            // 
+            javer.BackColor = Color.FromArgb(192, 255, 192);
+            javer.Location = new Point(116, 492);
+            javer.Name = "javer";
+            javer.Size = new Size(125, 27);
+            javer.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(29, 446);
+            label9.Name = "label9";
+            label9.Size = new Size(16, 20);
+            label9.TabIndex = 17;
+            label9.Text = "F";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(29, 382);
+            label8.Name = "label8";
+            label8.Size = new Size(20, 20);
+            label8.TabIndex = 16;
+            label8.Text = "D";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(29, 329);
+            label7.Name = "label7";
+            label7.Size = new Size(30, 20);
+            label7.TabIndex = 15;
+            label7.Text = "D+";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(29, 274);
+            label6.Name = "label6";
+            label6.Size = new Size(18, 20);
+            label6.TabIndex = 14;
+            label6.Text = "C";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(29, 218);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 20);
+            label5.TabIndex = 13;
+            label5.Text = "C+";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(29, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(18, 20);
+            label4.TabIndex = 12;
+            label4.Text = "B";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(29, 119);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 20);
+            label3.TabIndex = 11;
+            label3.Text = "B+";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Leelawadee UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(29, 499);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 20);
+            label2.TabIndex = 10;
+            label2.Text = "เกรดเฉลี่ย";
+            label2.Click += label2_Click;
+            // 
+            // tbgB1
+            // 
+            tbgB1.BackColor = SystemColors.InactiveCaption;
+            tbgB1.Location = new Point(116, 122);
+            tbgB1.Name = "tbgB1";
+            tbgB1.Size = new Size(125, 27);
+            tbgB1.TabIndex = 9;
+            // 
+            // tbgB2
+            // 
+            tbgB2.BackColor = SystemColors.InactiveCaption;
+            tbgB2.Location = new Point(116, 161);
+            tbgB2.Name = "tbgB2";
+            tbgB2.Size = new Size(125, 27);
+            tbgB2.TabIndex = 8;
+            // 
+            // tbgC1
+            // 
+            tbgC1.BackColor = SystemColors.InactiveCaption;
+            tbgC1.Location = new Point(116, 215);
+            tbgC1.Name = "tbgC1";
+            tbgC1.Size = new Size(125, 27);
+            tbgC1.TabIndex = 7;
+            // 
+            // tbgC2
+            // 
+            tbgC2.BackColor = SystemColors.InactiveCaption;
+            tbgC2.Location = new Point(116, 267);
+            tbgC2.Name = "tbgC2";
+            tbgC2.Size = new Size(125, 27);
+            tbgC2.TabIndex = 6;
+            // 
+            // tbgD1
+            // 
+            tbgD1.BackColor = SystemColors.InactiveCaption;
+            tbgD1.Location = new Point(116, 322);
+            tbgD1.Name = "tbgD1";
+            tbgD1.Size = new Size(125, 27);
+            tbgD1.TabIndex = 5;
+            // 
+            // tbgD2
+            // 
+            tbgD2.BackColor = SystemColors.InactiveCaption;
+            tbgD2.Location = new Point(116, 379);
+            tbgD2.Name = "tbgD2";
+            tbgD2.Size = new Size(125, 27);
+            tbgD2.TabIndex = 4;
+            // 
+            // tbgF
+            // 
+            tbgF.BackColor = SystemColors.InactiveCaption;
+            tbgF.Location = new Point(116, 439);
+            tbgF.Name = "tbgF";
+            tbgF.Size = new Size(125, 27);
+            tbgF.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 20);
+            label1.TabIndex = 0;
+            label1.Text = "A";
+            // 
+            // tbgA
+            // 
+            tbgA.BackColor = SystemColors.InactiveCaption;
+            tbgA.Location = new Point(116, 66);
+            tbgA.Name = "tbgA";
+            tbgA.Size = new Size(125, 27);
+            tbgA.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1386, 706);
+            Controls.Add(grad);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "กรอกข้อมูล";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            grad.ResumeLayout(false);
+            grad.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox grad;
+        private TextBox tbinputNAME;
+        private TextBox tbinputScore;
+        private Button button1;
+        private TextBox tbinputID;
+        private TextBox javer;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox tbgB1;
+        private TextBox tbgB2;
+        private TextBox tbgC1;
+        private TextBox tbgC2;
+        private TextBox tbgD1;
+        private TextBox tbgD2;
+        private TextBox tbgF;
+        private Label label1;
+        private TextBox tbgA;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label18;
+        private Label label17;
+        private Label label16;
+        private Label label15;
+        private Label label14;
+        private TextBox tbIDScore1;
+        private TextBox tbNameScore1;
+        private TextBox tbSCORE1;
+        private TextBox tbSCORE2;
+        private TextBox tbAVER;
+        private TextBox tbNameScore2;
+        private TextBox tbIDScore2;
+        private Label label10;
+    }
+}
---
+++ lab j2.2/Form1.cs
@@ -0,0 +1,163 @@
+namespace lab_j2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+        int arrayIndex = 0;
+        string[] arrID = new string[1000];
+        string[] arrName = new string[1000];
+        int[] arrScore = new int[10];
+
+        string inputID;
+        string inputNAME;
+        string inputScore;
+
+        int max_score, max_index, min_score, min_index;
+        //เก็บค่าคำนวณเกรด ตัวแปลไว้ข้างนอก โค้ดที่รันไว้ข้างใน
+        int GRAD_A = 0;
+        int GRAD_B = 0;
+        int GRAD_B2 = 0;
+        int GRAD_C = 0;
+        int GRAD_C2 = 0;
+        int GRAD_D = 0;
+        int GRAD_D2 = 0;
+        int GRAD_F = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //get input data from textbox
+            inputID = tbinputID.Text;
+            inputNAME = tbinputNAME.Text;
+            inputScore = tbinputScore.Text;
+            //create array สร้างเพื่อเก็บข้อมูลไว้
+
+            //add data to array
+            arrID[arrayIndex] = inputID;
+            arrName[arrayIndex] = inputNAME;
+            arrScore[arrayIndex] = int.Parse(inputScore);
+            //add data to array
+            arrayIndex++;
+
+            // array = [1 ,2 ,3 , 4 ]
+            //การหาค่ามากสุด
+            max_score = arrScore.Max();
+            max_index = arrScore.ToList().IndexOf(max_score);
+
+            tbSCORE1.Text = arrScore[max_index].ToString();
+            tbIDScore1.Text = arrID[max_index];
+            tbNameScore1.Text = arrName[max_index];
+
+
+            min_score = max_score;
+            for (int i = 0; i < arrayIndex; i++)
+            {
+
+                if (arrScore[i] < min_score)
+                {
+
+                    min_score = arrScore[i];
+                }
+
+            }
+
+            min_index = arrScore.ToList().IndexOf(min_score);
+
+
+
+
+            tbSCORE2.Text = arrScore[min_index].ToString();
+            tbIDScore2.Text = arrID[min_index].ToString();
+            tbNameScore2.Text = arrName[min_index].ToString();
+
+            int sum = 0;
+            for (int i = 0; i < arrayIndex; i++)
+            {
+
+                sum += arrScore[i];
+
+            }
+
+            tbAVER.Text = (sum / arrayIndex).ToString();
+
+            int SCORE = int.Parse(tbinputScore.Text);
+            if (SCORE >= 80 && SCORE <= 100)
+            {
+                GRAD_A += 1;
+                tbgA.Text = GRAD_A.ToString();
+
+
+            }
+
+            else if (SCORE >= 75 && SCORE <= 79)
+            {
+                GRAD_B += 1;
+                tbgB1.Text = GRAD_B.ToString();
+
+            }
+
+            else if (SCORE >= 70 && SCORE <= 74)
+            {
+                GRAD_B2 += 1;
+                tbgB2.Text = GRAD_B2.ToString();
+
+            }
+
+            else if (SCORE >= 65 && SCORE <= 69)
+            {
+                GRAD_C += 1;
+                tbgC1.Text = GRAD_C.ToString();
+
+
+            }
+
+            else if (SCORE >= 60 && SCORE <= 64)
+            {
+                GRAD_C2 += 1;
+                tbgC2.Text = GRAD_C2.ToString();
+
+            }
+
+            else if (SCORE <= 59 && SCORE >= 55)
+            {
+                GRAD_D += 1;
+                tbgD1.Text = GRAD_D.ToString();
+
+            }
+
+            else if (SCORE <= 54 && SCORE >= 50)
+            {
+                GRAD_D2 += 1;
+                tbgD2.Text = GRAD_D2.ToString();
+
+            }
+
+            else
+            {
+                GRAD_F += 1;
+                tbgF.Text = GRAD_F.ToString();
+            }
+
+            double GRA = ((GRAD_A * 4.00) + (GRAD_B * 3.50) + (GRAD_B2 * 3.00) + (GRAD_C * 2.50) + (GRAD_C2 * 2.00) + (GRAD_D * 1.50) + (GRAD_D2 * 1.00)) /
+                ((GRAD_A) + (GRAD_B) + (GRAD_B2) + (GRAD_C) + (GRAD_C2) + (GRAD_D) + (GRAD_D2));
+            javer.Text = String.Format("{0:F2}", GRA);
+        }
+    }
+}
